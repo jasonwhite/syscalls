b7e7d19a1a5da2a3c5e0c6ccb5f12d5c534b0c65
Yuri Astrakhan <yuriastrakhan@gmail.com>
2023-03-24 15:23:40 -0400

    Break syscalls-gen into few files (#35)

--- syscalls-gen/src/main.rs
+++ syscalls-gen/src/main.rs
@@ -1,18 +1,17 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![allow(clippy::upper_case_acronyms)]
 
-use futures::future::try_join_all;
-use std::borrow::Cow;
-use std::fmt;
-use std::fs::File;
-use std::future::Future;
-use std::io::Write;
-use std::path::{Path, PathBuf};
-use std::pin::Pin;
-
+use crate::tables::Source;
 use color_eyre::eyre::{eyre, Result, WrapErr};
+use futures::future::try_join_all;
 use lazy_static::lazy_static;
-use regex::Regex;
+use std::future::Future;
+use std::path::Path;
+use std::pin::Pin;
+use tables::{Header, Table};
+
+mod errors;
+mod tables;
 
 /// URL of the Linux repository to pull the syscall tables from.
 static LINUX_REPO: &str = "https://raw.githubusercontent.com/torvalds/linux";
@@ -92,7 +91,7 @@ lazy_static! {
     ];
 }
 
-struct ABI<'a> {
+pub struct ABI<'a> {
     name: &'a str,
     offset: u32,
 }
@@ -109,233 +108,9 @@ impl<'a> ABI<'a> {
     pub const O32: Self = Self::new("o32", 4000);
     pub const N64: Self = Self::new("n64", 5000);
 
+    #[must_use]
     pub const fn new(name: &'a str, offset: u32) -> Self {
         Self { name, offset }
-    }
-}
-
-struct Table<'a> {
-    arch: &'a str,
-    path: &'a str,
-    abi: &'a [ABI<'a>],
-}
-
-struct Header<'a> {
-    arch: &'a str,
-    headers: &'a [&'a str],
-    blocklist: &'a [&'a str],
-}
-
-enum Source<'a> {
-    /// The definitions are in a `syscall.tbl` file.
-    Table(Table<'a>),
-    /// The definitions are in a unistd.h header file.
-    Header(Header<'a>),
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct TableEntry {
-    id: u32,
-    name: String,
-    entry_point: Option<String>,
-}
-
-impl TableEntry {
-    fn ident(&self) -> Cow<str> {
-        if self.name.as_str() == "break" {
-            Cow::Owned(format!("r#{}", self.name))
-        } else {
-            Cow::Borrowed(&self.name)
-        }
-    }
-}
-
-impl<'a> Table<'a> {
-    async fn fetch_table(&self) -> Result<Vec<TableEntry>> {
-        let contents = fetch_path(self.path).await?;
-
-        let mut table = Vec::new();
-
-        for line in contents.lines() {
-            let line = line.trim();
-
-            // Skip over empty lines and comments.
-            if line.is_empty() || line.starts_with('#') {
-                continue;
-            }
-
-            let mut fields =
-                line.split(char::is_whitespace).filter(|x| !x.is_empty());
-
-            let id: u32 = fields
-                .next()
-                .ok_or_else(|| eyre!("Missing syscall number (line {line:?})"))?
-                .parse()
-                .wrap_err_with(|| eyre!("Failed parsing line {line:?}"))?;
-            let abi_name = fields.next().ok_or_else(|| {
-                eyre!("Missing syscall abi field (line {line:?})")
-            })?;
-            let name = fields
-                .next()
-                .ok_or_else(|| {
-                    eyre!("Missing syscall name field (line {line:?})")
-                })?
-                .into();
-            let entry_point = fields.next().map(Into::into);
-
-            for abi in self.abi {
-                if abi.name == abi_name {
-                    table.push(TableEntry {
-                        id: id + abi.offset,
-                        name,
-                        entry_point,
-                    });
-                    break;
-                }
-            }
-        }
-
-        // The table should already be sorted, but lets make sure.
-        table.sort();
-
-        Ok(table)
-    }
-}
-
-impl<'a> Header<'a> {
-    async fn fetch_table(&self) -> Result<Vec<TableEntry>> {
-        lazy_static! {
-            // Pattern for matching the syscall definition.
-            static ref RE_SYSCALLNR: Regex = Regex::new(r"^#define\s+__NR(?:3264)?_([a-z0-9_]+)\s+(\d+)").unwrap();
-        }
-
-        let mut table = Vec::new();
-
-        for header in self.headers {
-            let contents = fetch_path(header).await?;
-
-            for line in contents.lines() {
-                let line = line.trim();
-
-                if let Some(cap) = RE_SYSCALLNR.captures(line) {
-                    let name: &str = cap[1].into();
-                    let id: u32 = cap[2].parse()?;
-
-                    if name == "syscalls" {
-                        // This just keeps track of the number of syscalls in
-                        // the table and isn't a real syscall.
-                        continue;
-                    }
-
-                    if name == "arch_specific_syscall" {
-                        // This is a placeholder for a block of 16 syscalls
-                        // that are reserved for future use.
-                        continue;
-                    }
-
-                    if self.blocklist.contains(&name) {
-                        continue;
-                    }
-
-                    table.push(TableEntry {
-                        id,
-                        name: name.into(),
-                        entry_point: Some(format!("sys_{name}")),
-                    });
-                }
-            }
-        }
-
-        // The table should already be sorted, but lets make sure.
-        table.sort();
-
-        Ok(table)
-    }
-}
-
-impl<'a> Source<'a> {
-    pub fn arch(&self) -> &'a str {
-        match self {
-            Self::Table(table) => table.arch,
-            Self::Header(header) => header.arch,
-        }
-    }
-
-    async fn fetch_table(&self) -> Result<Vec<TableEntry>> {
-        match self {
-            Self::Table(table) => table.fetch_table().await,
-            Self::Header(header) => header.fetch_table().await,
-        }
-    }
-
-    /// Generates the source file.
-    async fn generate(&self, dir: &Path) -> Result<()> {
-        let arch = self.arch();
-        let table = self
-            .fetch_table()
-            .await
-            .wrap_err_with(|| eyre!("Failed fetching table for {arch}"))?;
-
-        // Generate `src/arch/{arch}.rs`
-        let path = dir.join(format!("src/arch/{arch}.rs"));
-
-        let mut file = File::create(&path).wrap_err_with(|| {
-            eyre!("Failed to create file {}", path.display())
-        })?;
-        writeln!(file, "//! Syscalls for the `{arch}` architecture.\n")?;
-        write!(file, "{}", SyscallFile(&table))?;
-
-        println!("Generated syscalls for {arch} at {}", path.display());
-        Ok(())
-    }
-}
-
-struct SyscallFile<'a>(&'a [TableEntry]);
-
-impl<'a> fmt::Display for SyscallFile<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "// This file is automatically generated. Do not edit!")?;
-        writeln!(f)?;
-
-        writeln!(f, "syscall_enum! {{")?;
-        writeln!(f, "    pub enum Sysno {{")?;
-        for entry in self.0 {
-            if entry.entry_point.is_some() {
-                writeln!(
-                    f,
-                    "        /// See [{name}(2)](https://man7.org/linux/man-pages/man2/{name}.2.html) for more info on this syscall.",
-                    name = entry.ident(),
-                )?;
-                writeln!(
-                    f,
-                    "        {name} = {id},",
-                    name = entry.ident(),
-                    id = entry.id
-                )?;
-            } else {
-                // This syscall has no entry point in the kernel, so we could
-                // technically exclude this from our list, but that will leave
-                // gaps in the syscall table. Our match statements can be better
-                // optimized by the compiler if we don't have gaps in the
-                // numbering.
-                writeln!(
-                    f,
-                    "        /// NOTE: `{name}` is not implemented in the kernel.",
-                    name = entry.ident(),
-                )?;
-                writeln!(
-                    f,
-                    "        {name} = {id},",
-                    name = entry.ident(),
-                    id = entry.id
-                )?;
-            }
-        }
-        writeln!(f, "    }}")?;
-        writeln!(f, "    LAST: {};", self.0.last().unwrap().ident())?;
-        writeln!(f, "}}")?;
-
-        Ok(())
     }
 }
 
@@ -354,133 +129,6 @@ async fn fetch_path(path: &str) -> Result<String> {
     Ok(contents)
 }
 
-async fn generate_errno(path: PathBuf) -> Result<()> {
-    let table = fetch_errno(&[
-        "include/uapi/asm-generic/errno-base.h",
-        "include/uapi/asm-generic/errno.h",
-        // error codes private to the Kernel, but are still useful when
-        // ptracing.
-        "include/linux/errno.h",
-    ])
-    .await?;
-
-    let mut file = File::create(&path)
-        .wrap_err_with(|| eyre!("Failed to create file {}", &path.display()))?;
-    write!(file, "{}", ErrnoFile(&table))?;
-
-    println!("Generated errno table at {}", &path.display());
-    Ok(())
-}
-
-#[derive(Debug, Clone)]
-enum Errno {
-    Definition {
-        name: String,
-        num: u32,
-        description: Option<String>,
-    },
-    #[allow(unused)]
-    Alias {
-        alias: String,
-        name: String,
-        description: Option<String>,
-    },
-}
-
-async fn fetch_errno(paths: &[&str]) -> Result<Vec<Errno>> {
-    let futures: Vec<_> = paths.iter().map(|path| fetch_path(path)).collect();
-
-    let mut errnos = Vec::new();
-    for content in try_join_all(futures).await? {
-        parse_errno(&content, &mut errnos)?;
-    }
-
-    Ok(errnos)
-}
-
-fn parse_errno(contents: &str, errnos: &mut Vec<Errno>) -> Result<()> {
-    lazy_static! {
-        // Pattern for matching the errno definition
-        static ref RE_DEFINITION: Regex = Regex::new(r"^#define\s+(E\w+)\s+(\d+)(?:\s+/\*([^\\*]+)\*/)?").unwrap();
-        // Pattern for matching errno aliases
-        static ref RE_ALIAS: Regex = Regex::new(r#"^#define\s+(E\w+)\s+(E\w+)(?:\s+/\*([^\\*]+)\*/)?"#).unwrap();
-    }
-
-    for line in contents.lines() {
-        if let Some(cap) = RE_DEFINITION.captures(line) {
-            let name = cap[1].into();
-            let num: u32 = cap[2].parse()?;
-            let description = cap.get(3).map(|m| m.as_str().trim().to_string());
-
-            errnos.push(Errno::Definition {
-                name,
-                num,
-                description,
-            });
-        } else if let Some(cap) = RE_ALIAS.captures(line) {
-            let alias = cap[1].into();
-            let name = cap[2].into();
-            let description = cap.get(3).map(|m| m.as_str().trim().to_string());
-
-            errnos.push(Errno::Alias {
-                alias,
-                name,
-                description,
-            });
-        }
-    }
-
-    Ok(())
-}
-
-struct ErrnoFile<'a>(&'a Vec<Errno>);
-
-impl<'a> fmt::Display for ErrnoFile<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "// This file is automatically generated. Do not edit!")?;
-        writeln!(f)?;
-
-        writeln!(f, "errno_enum! {{")?;
-        writeln!(f, "    pub enum Errno {{")?;
-
-        for value in self.0.iter() {
-            match value {
-                Errno::Definition {
-                    name,
-                    num,
-                    description,
-                } => {
-                    let description = description.as_ref().map_or_else(
-                        || {
-                            // Try to make a best-effort guess for error codes that
-                            // don't have a description.
-                            match name.as_str() {
-                                "ERESTARTSYS" => "Restart syscall",
-                                "ERESTARTNOINTR" => "Restart if no interrupt",
-                                _ => panic!(
-                                    "Could not find a description for {name}"
-                                ),
-                            }
-                        },
-                        |s| s.as_str(),
-                    );
-
-                    writeln!(f, r#"        {name}({num}) = "{description}","#)?;
-                }
-                Errno::Alias { .. } => {
-                    // Don't include aliases since it makes our macro more
-                    // complicated. We can add these manually later.
-                }
-            }
-        }
-
-        writeln!(f, "    }}")?;
-        writeln!(f, "}}")?;
-
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
@@ -495,7 +143,7 @@ async fn main() -> Result<()> {
     }
 
     let errno = base_dir.join("src/errno/generated.rs");
-    futures.push(Box::pin(generate_errno(errno)));
+    futures.push(Box::pin(errors::generate_errno(errno)));
 
     try_join_all(futures).await?;
 
